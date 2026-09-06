bbf5d3d8d08c3b999bbb6f26659bd91e4e43a3ed
Michail Maltsev <mm.e.mihon.com>
2026-09-06 13:55:50 +0300

    example env file

--- src/later_api/config.py
+++ src/later_api/config.py
@@ -4,5 +4,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+    class Config:
+        env_file = "../../.env"  # Поднимаемся на уровень выше в корневую директорию
+        env_file_encoding = "utf-8"
+        extra = "ignore"  # Игнорируем лишние поля
+
 
 settings = Settings()
