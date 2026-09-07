60d75cdbaee7e8e443162edd10ebbad7d668bda6
Michail Maltsev <mm.e.mihon.com>
2026-09-07 23:14:08 +0300

    import fix

--- src/later_api/database/database.py
+++ src/later_api/database/database.py
@@ -3,8 +3,8 @@ from typing import Annotated
 from fastapi import Depends
 from sqlmodel import Session, SQLModel, create_engine
 
-from later_api.config import settings
-from later_api.models.resources import (
+from src.later_api.config import settings
+from src.later_api.models.resources import (
     Resource,  # pyright: ignore[reportUnusedImport]  # noqa: F401
 )
 
